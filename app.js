8ba512759b9b910ebc3a4482b39935536acf267f
SAI2101 <saiabhang866@gmail.com>
2025-11-22 11:19:50 +0530

    Create App component with OcrExtractor

---
+++ app.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import OcrExtractor from './OcrExtractor'; 
+
+
+function App() {
+  return (
+    <div className="App">
+      {/* Render the main text extraction component */}
+      <OcrExtractor />
+    </div>
+  );
+}
+
+export default App;
